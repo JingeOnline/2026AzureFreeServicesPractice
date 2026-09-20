d6594cc39f946edc31fc01fc6e7908e56eac309c
Jinge <jinge_windows@hotmail.com>
2026-09-20 22:57:29 +1000

    Add project files.

---
+++ CosmosDbSampleData/Program.cs
@@ -0,0 +1,88 @@
+﻿using SecretLibrary;
+using Microsoft.Azure.Cosmos;
+using Azure.Identity;
+
+namespace CosmosDbSampleData
+{
+    internal class Program
+    {
+        static string SecretConnectionString = "AzureCosmo-SampleDbConnectionString";
+        static string SecretAccountUrl = "AzureCosmo-SampleDbAccountUrl";
+        static string AzureCosmoPrimaryKey = "AzureCosmo-SampleKey";
+
+
+        static async Task Main(string[] args)
+        {
+            CosmosClient client = await ConnectUsingPrimaryKey();
+            await VerifyExist(client);
+        }
+
+        /// <summary>
+        /// 使用连接字符串连接到Cosmos DB
+        /// </summary>
+        /// <returns></returns>
+        static async Task<CosmosClient> ConnectUsingConnectionString()
+        {
+            string? connectionString = OneDriveSecretFileHelper.getJsonConfig(SecretConnectionString);
+            CosmosClient client = new CosmosClient(connectionString);
+            //获取账户属性，验证连接是否成功
+            AccountProperties account = await client.ReadAccountAsync();
+            Console.WriteLine($"成功连接！账户 ID 为: {account.Id}");
+            return client;
+        }
+
+        /// <summary>
+        /// 使用cosmos db account url和主密钥连接到Cosmos DB
+        /// </summary>
+        /// <returns></returns>
+        static async Task<CosmosClient> ConnectUsingPrimaryKey()
+        {
+            DefaultAzureCredential credential = new DefaultAzureCredential();
+            string? accountUrl = OneDriveSecretFileHelper.getJsonConfig(SecretAccountUrl);
+            string? primaryKey = OneDriveSecretFileHelper.getJsonConfig(AzureCosmoPrimaryKey);
+            CosmosClient client = new CosmosClient(accountUrl, primaryKey);
+            //获取账户属性，验证连接是否成功
+            AccountProperties account = await client.ReadAccountAsync();
+            Console.WriteLine($"成功连接！账户 ID 为: {account.Id}");
+            return client;
+        }
+
+        /// <summary>
+        /// 验证数据库和容器是否存在
+        /// </summary>
+        /// <param name="client"></param>
+        /// <returns></returns>
+        static async Task VerifyExist(CosmosClient client)
+        {
+            //获取数据库对象（即使不存在也会被获取到）
+            Database databaseTest = client.GetDatabase("随便什么名称");
+            Console.WriteLine($"成功获取数据库！数据库名称为: {databaseTest.Id}");
+            //验证云端是否存在该数据库
+            Database database = client.GetDatabase("SampleDB");
+            try
+            {
+                DatabaseProperties databaseProperties = await database.ReadAsync();
+                Console.WriteLine($"数据库存在！数据库名称为: {databaseProperties.Id}");
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("数据库不存在！");
+            }
+            //获取容器对象（相当于表）（即使不存在也会被获取到）
+            Container containerTest = database.GetContainer("无论什么名称，该容器是否存在，都会被获取到。");
+            Console.WriteLine($"成功获取容器！容器名称为: {containerTest.Id}");
+            //验证云端是否存在该容器
+            Container container = database.GetContainer("SampleContainer");
+            try
+            {
+                ContainerProperties containerProperties = await container.ReadContainerAsync();
+                Console.WriteLine($"容器存在！容器名称为: {containerProperties.Id}");
+            }
+            catch (CosmosException ex) when (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("容器不存在！");
+            }
+        }
+
+    }
+}
---
+++ SecretLibrary/OneDriveSecretFileHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace SecretLibrary
+{
+    /// <summary>
+    /// A helper class to read secrets from a JSON file located in the user's OneDrive folder.
+    /// </summary>
+    public sealed class OneDriveSecretFileHelper
+    {
+        private static readonly string secretFileSubPath = "OneDrive\\脚本代码\\AccountSecrets.json";
+        private static readonly string secretFilePath = GetSecretFilePath();
+
+        //使用Lazy<T>创建的变量，其内部的匿名函数只会在第一次访问Value属性时执行一次，之后的访问将直接返回已经创建的实例。
+        private static readonly Lazy<IConfiguration> config = new(() =>
+            new ConfigurationBuilder().AddJsonFile(secretFilePath).Build());
+
+        private static string GetSecretFilePath()
+        {
+            string userFolder = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+            return Path.Combine(userFolder, secretFileSubPath);
+        }
+
+        /// <summary>
+        /// 返回指定的key对应的值，如果key不存在，则返回null
+        /// </summary>
+        /// <param name="key">读取嵌套的子元素的值的时候，中间用冒号连接</param>
+        /// <returns></returns>
+        public static string? getJsonConfig(string key)
+        {
+            var section = config.Value.GetSection(key);
+            return section.Value;
+        }
+
+        /// <summary>
+        /// 返回指定的key:subKey对应的值，如果该键不存在，则返回null
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="subKey"></param>
+        /// <returns></returns>
+        public static string? getJsonConfig(string key, string subKey)
+        {
+            var section = config.Value.GetSection(key).GetSection(subKey);
+            return section.Value;
+        }
+    }
+}
